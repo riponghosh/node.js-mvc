d80bbbe1f8433c4d64b5e2bf34dcad8c35fe6caa
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-02-23 21:46:28 +0600

    route distribute

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var router = require('./routes/routes');
 
 var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', router);
 
 require('./config/static-files')(app,express);
 var db=require('./config/database');
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var homeController=require('../controllers/homeController');
-
 /* GET home page. */
 router.get('/', homeController.index);
 
