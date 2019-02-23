6ecd5ade89a9e177d11e6ea612a37b9634203cd1
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-02-23 21:30:17 +0600

    view edit for get html file direct after /  url

--- config/static-files.js
+++ config/static-files.js
@@ -1,5 +1,6 @@
 var path = require('path');
 module.exports = function (app,express){
 	app.use('/css',express.static(path.join(__dirname, '../public/stylesheets')));
-	app.use('/views', express.static(path.join(__dirname, '../views')));//you can access views folder file as localhost:3000/views/login.html
+	//app.use('/views', express.static(path.join(__dirname, '../views')));//you can access views folder file as localhost:3000/views/login.html
+	app.use('/', express.static(path.join(__dirname, '../views')));//you can access views folder file as localhost:3000/login.html , can also use route to open login.html
 }
